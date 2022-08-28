7b2bc48035a32573d46be8840957b1a29337ca57
chanshu <chanshukumar.ck@gmail.com>
2022-08-28 22:27:52 +0530

    frontent updated

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,9 +1,6 @@
 let name = "You";
 let textarea = document.querySelector('#textarea')
 let messageArea = document.querySelector('.message__area')
-// do {
-//     name = prompt('Please enter your name: ')
-// } while(!name)
 
 textarea.addEventListener('keyup', (e) => {
     if(e.key === 'Enter') {
@@ -22,7 +19,29 @@ function sendMessage(message) {
     scrollToBottom()
 
     // Send to server 
-    // socket.emit('message', msg)
+    const data = { q: message.trim() };
+    const apiUrl = 'https://chanshu19.pythonanywhere.com/api/respond'
+    fetch(apiUrl, {
+    method: 'POST', // or 'PUT'
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data['answer'])
+        let msg = {
+            user: "Sudarshan",
+            message: data['answer']
+        }
+        appendMessage(msg, 'incoming')
+        scrollToBottom()
+    })
+    .catch((error) => {
+        console.log('Error:', error);
+    });
+
 
 }
 
@@ -38,12 +57,6 @@ function appendMessage(msg, type) {
     mainDiv.innerHTML = markup
     messageArea.appendChild(mainDiv)
 }
-
-// Recieve messages 
-// socket.on('message', (msg) => {
-//     appendMessage(msg, 'incoming')
-//     scrollToBottom()
-// })
 
 function scrollToBottom() {
     messageArea.scrollTop = messageArea.scrollHeight
